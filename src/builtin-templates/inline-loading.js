af385e3854c65511818341c910064fb696956cec
kyungeunni <kyungeunni@gmail.com>
2019-06-03 13:25:00 +0100

    rename spnner classes, remove unnecessary prefixes

--- src/builtin-templates/inline-loading.js
+++ src/builtin-templates/inline-loading.js
@@ -2,15 +2,15 @@ import { html } from '../lit-html';
 
 export default () => html`
 <div class="inline-loading">
-    <div class="sk-fading-circle">
-        <div class="sk-circle1 sk-circle"></div>
-        <div class="sk-circle2 sk-circle"></div>
-        <div class="sk-circle3 sk-circle"></div>
-        <div class="sk-circle4 sk-circle"></div>
-        <div class="sk-circle5 sk-circle"></div>
-        <div class="sk-circle6 sk-circle"></div>
-        <div class="sk-circle7 sk-circle"></div>
-        <div class="sk-circle8 sk-circle"></div>
+    <div class="spinner">
+        <div class="spinner__bar1 spinner__bar"></div>
+        <div class="spinner__bar2 spinner__bar"></div>
+        <div class="spinner__bar3 spinner__bar"></div>
+        <div class="spinner__bar4 spinner__bar"></div>
+        <div class="spinner__bar5 spinner__bar"></div>
+        <div class="spinner__bar6 spinner__bar"></div>
+        <div class="spinner__bar7 spinner__bar"></div>
+        <div class="spinner__bar8 spinner__bar"></div>
     </div>
 
     <span>Please wait a moment</span>
