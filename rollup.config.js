ed5c45540789105cc0bef0ee36fba2d4b580f50b
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-06-04 17:34:35 +0100

    Updates build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 const alias = require('rollup-plugin-alias');
 const p = require('./package');
-const { join } = require('path');
 const aliases = {};
 
 for (const m of [...p['@pika/web'].webDependencies, ...p['hack-wrap']]) {
-    aliases[`/web_modules/${m}.js`] = join(__dirname, 'web_modules', `${m}.js`);
+    aliases[`/web_modules/${m}.js`] = `web_modules/${m}.js`;
 }
 
 module.exports = {
