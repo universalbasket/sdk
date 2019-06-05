6fb78a91434285098f39accecdcfd313085e8797
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-06-05 16:30:55 +0100

    Enhanced custom action script.

--- .github/create-release/entrypoint.sh
+++ .github/create-release/entrypoint.sh
@@ -5,5 +5,15 @@ echo "Creating release for $GITHUB_REF."
 # Trim leading refs/tags/ and assign to TAG_NAME.
 
 TAG_NAME=${GITHUB_REF:10}
+DATA='{"name": "$TAG_NAME", "tag_name": "$TAG_NAME"}'
 
-curl --data '{"name": "$TAGNAME", "tag": "$TAGNAME"}' https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$GITHUB_TOKEN
+echo $DATA
+
+URL=https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$GITHUB_TOKEN
+
+status=$(curl -k -s -o /dev/null -w "%{http_code}" --data $DATA $URL)
+
+if [ "$status" -ge 300 ]; then
+    >&2 echo "Error, got status $status."
+    exit 1
+fi
