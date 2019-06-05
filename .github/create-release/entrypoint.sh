ac91f777986b2fcc8955ca4e14d519aa1776e0ef
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-06-05 16:20:15 +0100

    Corrects variable substitution in custom action.

--- .github/create-release/entrypoint.sh
+++ .github/create-release/entrypoint.sh
@@ -4,6 +4,6 @@ echo "Creating release for $GITHUB_REF."
 
 # Trim leading refs/tags/ and assign to TAG_NAME.
 
-TAG_NAME=${$GITHUB_REF:10}
+TAG_NAME=${GITHUB_REF:10}
 
 curl --data '{"name": "$TAGNAME", "tag": "$TAGNAME"}' https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$GITHUB_TOKEN
