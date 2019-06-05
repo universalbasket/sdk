f33c6d222f49a9a214d3a7b0d38d4a1a05d91803
muzafarova <alisapsee@gmail.com>
2019-06-05 18:24:12 +0100

    Display price

---
+++ src/builtin-templates/price-display.js
@@ -0,0 +1,14 @@
+import { html } from '/web_modules/lit-html.js';
+import getSymbolFromCurrency from '/web_modules/currency-symbol-map.js'
+
+const template = (price) => {
+    if (!price) {
+        return html`&middot;`
+    }
+    const {value, currencyCode} = price;
+    return value === 0 ?
+        html`FREE` :
+        html`${getSymbolFromCurrency(currencyCode.toUpperCase())}${value}`
+}
+
+export default template
