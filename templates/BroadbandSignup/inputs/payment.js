f508efa6d7c661c6a129d3c7d041576e87fbbdd7
kyungeunni <kyungeunni@gmail.com>
2019-06-05 09:03:55 +0100

    add css for hosted form

--- templates/BroadbandSignup/inputs/payment.js
+++ templates/BroadbandSignup/inputs/payment.js
@@ -5,18 +5,22 @@ import Person from '../../Generic/Person';
 const baseUrl = 'https://vault.automationcloud.net/forms/index.html';
 
 //check available options: https://docs.automationcloud.net/docs/vaulting-payment-card#section-styling-and-configuring-the-form
-const fields = 'pan,name,expiry-select,cvv';
+const fields = 'pan,name,expiry-select,cvv_cvv';
 const brands = 'visa,mastercard';
 const validateOnInput = 'on';
+const css = 'https://kk-iframe-prd.glitch.me/style.css'; // todo: host it somewhere proper.
 
 export default (otp) => html`
+    <h2>Card details</h2>
     <iframe
         id="vault-iframe"
         width="400"
-        height="350"
-        src="${baseUrl}?otp=${otp}&fields=${fields}&brands=${brands}&validateOnInput=${validateOnInput}">
+        height="280"
+        scrolling="no"
+        src="${baseUrl}?otp=${otp}&css=${css}&fields=${fields}&brands=${brands}&validateOnInput=${validateOnInput}">
     </iframe>
 
+    <h2>Billing Address</h2>
     ${Address('payment[address]')}
     ${Person('payment[person]')}
 `;
--- templates/Generic/Address.js
+++ templates/Generic/Address.js
@@ -4,33 +4,33 @@ export default (prefix = 'address') => html`
 <div name="${prefix}" class="filed-set">
     <div class="field">
         <label for="${prefix}[line1]" class="field__name">Line 1</label>
-        <input type="text" name="${prefix}[line1]" id="${prefix}[line1]" value="587" required />
+        <input type="text" name="${prefix}[line1]" id="${prefix}[line1]" required />
     </div>
 
     <div class="field">
         <label for="${prefix}[line2]" class="field__name">Line 2</label>
-        <input type="text" name="${prefix}[line2]" id="${prefix}[line2]" value="high road" />
+        <input type="text" name="${prefix}[line2]" id="${prefix}[line2]" />
     </div>
 
     <div class="field">
         <label for="${prefix}[city]" class="field__name">City</label>
-        <input type="text" name="${prefix}[city]" id="${prefix}[city]" value="london" required />
+        <input type="text" name="${prefix}[city]" id="${prefix}[city]" required />
     </div>
 
     <div class="field">
         <label for="${prefix}[country-subdivision]" class="field__name">County</label>
-        <input type="text" name="${prefix}[country-subdivision]" id="${prefix}[country-subdivision]" value="London" required/>
+        <input type="text" name="${prefix}[country-subdivision]" id="${prefix}[country-subdivision]" required/>
     </div>
 
     <div class="field">
     <!-- select -->
         <label for="${prefix}[country-code]" class="field__name">Country Code</label>
-        <input type="text" name="${prefix}[country-code]" id="${prefix}[country-code]" value="gb" required />
+        <input type="text" name="${prefix}[country-code]" id="${prefix}[country-code]" required />
     </div>
 
     <div class="field">
         <label for="${prefix}[postcode]" class="field__name">Postcode</label>
-        <input type="text" name="${prefix}[postcode]" id="${prefix}[postcode]" value="E11 4PB" required />
+        <input type="text" name="${prefix}[postcode]" id="${prefix}[postcode]" minlength="6" required/>
     </div>
 </div>
 `;
