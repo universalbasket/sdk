b8440777ad13140a3f1268482ebd92ba4be4d0b9
kyungeunni <kyungeunni@gmail.com>
2019-06-06 13:12:24 +0100

    add domain selector for dev

--- index.example.js
+++ index.example.js
@@ -1,5 +1,5 @@
 import { createApp } from './src/main.js';
-import CONFIG from './templates/BroadbandSignup/ubio.config.js';
+import CONFIG from './templates/$DOMAIN/ubio.config.js';
 
 // I'd like this part is done by script
 // if there's an error(i.e. Template for ... not found), it stops build
@@ -7,8 +7,8 @@ import CONFIG from './templates/BroadbandSignup/ubio.config.js';
 
 // TODO: ideally the directory is will be written in the config
 // or we designate the directory like below and force them to add the templates under the directory
-import * as LayoutTemplates from './templates/BroadbandSignup/layout/index.js';
-import * as SectionTemplates from './templates/BroadbandSignup/sections/index.js';
+import * as LayoutTemplates from './templates/$DOMAIN/layout/index.js';
+import * as SectionTemplates from './templates/$DOMAIN/sections/index.js';
 // confirmation, 404, error page will be added
 
 const Layout = CONFIG.layout.map(l => {
