9fef4ecb84d1ef056bd0dc301e605ee0d9187842
kyungeunni <kyungeunni@gmail.com>
2019-06-06 16:27:21 +0100

    update lit-html module directory in PetInsurance templates

--- templates/PetInsurance/sections/excess.js
+++ templates/PetInsurance/sections/excess.js
@@ -1,4 +1,4 @@
-import { html } from '/web_modules/lit-html.js';
+import { html } from '/web_modules/lit-html/lit-html.js';
 import { selectedVoluntaryExcess } from '../inputs/index.js';
 
 export default (name, { availableVoluntaryExcesses }) => html`
--- templates/PetInsurance/sections/payment-terms.js
+++ templates/PetInsurance/sections/payment-terms.js
@@ -1,4 +1,4 @@
-import { html } from '/web_modules/lit-html.js';
+import { html } from '/web_modules/lit-html/lit-html.js';
 import { selectedPaymentTerm, selectedVetPaymentTerm } from '../inputs/index.js';
 
 export default (name, { availablePaymentTerms }) => html`
