df93b8b28b29eef27f881c2115e050e87880c536
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-06-11 16:09:06 +0100

    Moves not-found template to built-ins.

--- src/render-not-found.js
+++ src/render-not-found.js
@@ -1,5 +1,5 @@
 import { render as lrender } from '/web_modules/lit-html/lit-html.js';
-import notFount404 from '../templates/not-found-404.js';
+import notFount404 from './builtin-templates/not-found-404.js';
 
 export default function(selector) {
     return {
