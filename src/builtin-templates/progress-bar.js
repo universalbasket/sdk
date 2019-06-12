f79eb447b243683ee07634c42536d5ffb0dce66e
muzafarova <alisapsee@gmail.com>
2019-06-12 17:41:22 +0100

    Fix

--- src/builtin-templates/progress-bar.js
+++ src/builtin-templates/progress-bar.js
@@ -20,7 +20,7 @@ function StepLabel(titles, activeIndex) {
     let rightRuler = activeIndex + 2;
     let textAlign = 'center';
 
-    if (titles[activeIndex].length > 14) {
+    if (titles[activeIndex - 1].length > 14) {
         leftRuler = activeIndex - 2;
         rightRuler = activeIndex + 3;
     }
