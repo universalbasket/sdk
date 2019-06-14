b82b0201eab17fc1490e280adcdcbc37e079824e
kyungeunni <kyungeunni@gmail.com>
2019-06-14 13:18:06 +0100

    update get-data-for-section, update selectedaddress for broadband

--- src/get-data-for-section.js
+++ src/get-data-for-section.js
@@ -1,47 +1,41 @@
 import getData from './get-data-with-priority.js';
 import * as Storage from './storage.js';
 
-export default function getDataForSection(waitFor = []) {
+export default async function getDataForSection(waitFor = []) {
     if (!waitFor || waitFor.length === 0) {
-        return Promise.resolve({});
+        return {};
     }
 
-    return new Promise(res => {
-        const results = waitFor.map(_ => {
-            const [type, sourceKey] = _.split('.');
-            if (type === 'input') {
-                const data = Storage.get('input', sourceKey);
-                return { data, wait: false, sourceKey };
-            }
-
-            const data = getData(type, sourceKey);
-            if (data === null) { // data is explicitly null
-                return { data: null, wait: false, sourceKey };
-            }
-
-            if (data) {
-                return { data, wait: false, sourceKey };
-            }
-
-            return { data: null, wait: true, sourceKey };
-        });
-
-        const keysToWaitFor = results.filter(r => r.wait === true).map(r => r.sourceKey);
-
-        if (keysToWaitFor.length === 0) {
-            const dataWaitFor = {};
-            results.forEach(result => { dataWaitFor[result.sourceKey] = result.data; });
-
-            return res(dataWaitFor);
+    const results = waitFor.map(_ => {
+        const [type, sourceKey] = _.split('.');
+        if (type === 'input') {
+            const data = Storage.get('input', sourceKey);
+            return { data, wait: false, sourceKey };
         }
 
+        const data = getData(type, sourceKey);
+        if (data === null || data) { // data is explicitly null
+            return { data, wait: false, sourceKey };
+        }
+
+        return { data, wait: true, sourceKey };
+    });
+
+    const keysToWaitFor = results.filter(r => r.wait === true).map(r => r.sourceKey);
+
+    if (keysToWaitFor.length === 0) {
         const dataWaitFor = {};
         results.forEach(result => { dataWaitFor[result.sourceKey] = result.data; });
 
-        waitForOutputs(keysToWaitFor).then(data => {
-            res({ ...dataWaitFor, ...data });
-        });
-    });
+        return dataWaitFor;
+    }
+
+    const dataWaitFor = {};
+    results.forEach(result => { dataWaitFor[result.sourceKey] = result.data; });
+
+    const data = await waitForOutputs(keysToWaitFor);
+
+    return { ...dataWaitFor, ...data };
 }
 
 function waitForOutputs(keysToWaitFor) {
