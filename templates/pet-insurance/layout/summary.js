ecc1ddebb3e4bbf304f8b20f22ce591d85e36c59
muzafarova <alisapsee@gmail.com>
2019-06-19 20:59:30 +0100

    Removes highlight from price displayed

--- templates/pet-insurance/layout/summary.js
+++ templates/pet-insurance/layout/summary.js
@@ -34,7 +34,7 @@ function SummaryDetails(inputs, outputs, cache) {
 
         ${ price ? html`
             <div class="summary__block summary__price">
-                <b class="large summary__highlight">
+                <b class="large">
                     ${ templates.priceDisplay(price) }
                 </b>
             </div>` :
