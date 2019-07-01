9c29f1f0d99c83520ac76a4bef947bb27026a123
muzafarova <alisapsee@gmail.com>
2019-07-01 09:47:01 +0100

    Do not show real error message to a user

--- src/builtin-templates/flash-error.js
+++ src/builtin-templates/flash-error.js
@@ -1,5 +1,4 @@
 import { html, render } from '/web_modules/lit-html/lit-html.js';
-import { unsafeHTML } from '/web_modules/lit-html/directives/unsafe-html.js';
 
 export default function create(error) {
     const el = document.querySelector('#flash-error');
@@ -9,17 +8,15 @@ export default function create(error) {
     }
 
     return {
-        show: () => { render(wrap(error), el); },
+        show: () => { render(template(), el); },
         hide: () => { render('', el); }
     };
 }
 
-function wrap(err) {
-    const message = typeof err === 'string' ? err : 'Please check the items in red below.';
-
+function template() {
     return html`
         <div class="flash-error">
             <p><b class="large">We’re sorry, something is missing or wrong.</b></p>
-            <p>${ unsafeHTML(message) }</p>
+            <p>Please check the items in red below.</p>
         </div>`;
 }
