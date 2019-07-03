640b6a111c76d11c40b2404289fc22da3d341377
muzafarova <alisapsee@gmail.com>
2019-07-03 23:00:39 +0100

    Clean up hardcoded values

--- templates/generic/person.js
+++ templates/generic/person.js
@@ -3,27 +3,47 @@ import { html } from '/src/main.js';
 const TITLES = ['mr', 'ms', 'mrs', 'miss'];
 
 export default (prefix = 'person') => html`
-<div name="${prefix}" class="filed-set">
-    <div class="field">
-        <label class="field__name">Title</label>
-        <select name="${prefix}[title]">
-            ${ TITLES.map(t => html`<option value="${t}"> ${ t.toUpperCase() }</option>`) }
-        </select>
-    </div>
+    <div name="${prefix}" class="filed-set">
+        <div class="field">
+            <label class="field__name">Title</label>
+            <select
+                name="${prefix}[title]"
+                required>
+                ${ TITLES.map(t => html`<option value="${t}"> ${ t.toUpperCase() }</option>`) }
+            </select>
+        </div>
 
-    <div class="field">
-        <label class="field__name" for="${prefix}[first-name]">First Name</label>
-        <input type="text" name="${prefix}[first-name]" placeholder="Jane" required />
-    </div>
+        <div class="field">
+            <label
+                class="field__name"
+                for="${prefix}[first-name]">First Name</label>
+            <input
+                type="text"
+                name="${prefix}[first-name]"
+                pattern="^[A-Za-z]+$"
+                data-error="First name should only contain letters (A-Z and a-z)"
+                required />
+        </div>
 
-    <div class="field">
-        <label class="field__name" for="${prefix}[middle-name]">Middle Name</label>
-        <input type="text" name="${prefix}[middle-name]" placeholder="" />
-    </div>
+        <div class="field">
+            <label
+                class="field__name"
+                for="${prefix}[middle-name]">Middle Name</label>
+            <input
+                type="text"
+                name="${prefix}[middle-name]" />
+        </div>
 
-    <div class="field">
-        <label class="field__name" for="${prefix}[last-name]">Last Name</label>
-        <input type="text" name="${prefix}[last-name]" placeholder="Doe" required />
+        <div class="field">
+            <label
+                class="field__name"
+                for="${prefix}[last-name]">Last Name</label>
+            <input
+                type="text"
+                name="${prefix}[last-name]"
+                pattern="^[A-Za-z]+$"
+                data-error="Last name should only contain letters (A-Z and a-z)"
+                required />
+        </div>
     </div>
-</div>
-`;
+    `;
