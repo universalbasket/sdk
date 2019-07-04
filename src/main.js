3068db58af40c59f41d60e24be0124f1812e9758
muzafarova <alisapsee@gmail.com>
2019-07-04 14:29:49 +0100

    Get rid of lit-html is error.js

--- src/main.js
+++ src/main.js
@@ -87,16 +87,8 @@ export async function createApp({ mountPoint, pages, cache = [], layout, error, 
     //setup router
     const routingOrder = pages.map(page => page.route);
 
-    function errorPageRenderer(sdk) {
-        return {
-            init() {
-                render(html`<div class="page"><div class="page__body" id="target">${error(sdk)}</div></div>`, document.querySelector(mainSelector));
-            }
-        };
-    }
-
     const routes = {
-        '/error': { renderer: errorPageRenderer(sdk), title: null, step: null }
+        '/error': { renderer: error(mainSelector, sdk), title: null, step: null }
     };
 
     pages.forEach(({ title, sections, route, excludeStep }, stepIndex) => {
--- templates/broadband-signup/error.js
+++ templates/broadband-signup/error.js
@@ -1,5 +1,3 @@
-import { html, until } from '/src/main.js';
-
 const emailAddress = 'configurable@emailaddre.ss';
 const phoneNumber = '0000 000 0000';
 const domain = new URLSearchParams(window.location.search).get('domain');
@@ -18,14 +16,25 @@ const errorMessage = {
 
 const defaultErrorMessage = `You can retry or get in touch with us at ${emailAddress}.`;
 
-async function Template(sdk) {
-    const { error } = await sdk.getJob();
+export default (selector, sdk) => {
+    return {
+        init() {
+            const element = document.querySelector(selector);
 
-    return html`
-        <p class="large"><b>We’re sorry. We can’t continue your purchase at the moment.</b></p>
-        <p class="dim">${errorMessage[error.code] || defaultErrorMessage}</p>
-        <p><a href="${url}" class="button button--primary">Retry</a></p>
-    `;
-}
+            element.innerHTML = `
+                <p class="large"><b>We’re sorry. We can’t continue your purchase at the moment.</b></p>
+                <p class="dim" id="message"></p>
+                <p><a href="${url}" class="button button--primary">Retry</a></p>
+            `;
 
-export default sdk => until(Template(sdk));
+            sdk.getJob().then(
+                job => {
+                    const message = errorMessage[job.error.code] || defaultErrorMessage;
+                    element.querySelector('#message').append(message);
+                },
+                error => console.warn('Error fetching job:', error.stack)
+            );
+        }
+    };
+};
+
