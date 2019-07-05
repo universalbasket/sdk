e2830f7d1e430f27f51e47a0f50b23a5ad2d90f5
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-07-05 11:52:47 +0100

    Allows PJO requests in parallel.

--- src/cache.js
+++ src/cache.js
@@ -6,29 +6,40 @@ function has(obj, key) {
     return Object.prototype.hasOwnProperty.call(obj, key);
 }
 
+function inputsSatisfied(sourceInputKeys, inputs) {
+    return sourceInputKeys.every(key => has(inputs, key));
+}
+
+async function getAndCachePreviousOutput(sdk, sourceInputs, outputKey) {
+    try {
+        console.log('Fetching previous job outputs for:', outputKey);
+
+        fetchingKeys.add(outputKey);
+
+        const result = await sdk.getPreviousJobOutputs(sourceInputs, outputKey);
+        const cache = result.data && result.data[0];
+
+        if (cache) {
+            Storage.set('cache', cache.key, cache.data);
+        }
+    } catch (error) {
+        console.error('Failed to fetch previous output:', error.stack || error.message);
+    } finally {
+        fetchingKeys.delete(outputKey);
+    }
+}
+
 export async function populate(sdk, CACHE_CONFIG) {
     const { inputs, cache } = Storage.getAll();
+    const promises = [];
 
     for (const { key: outputKey, sourceInputKeys } of CACHE_CONFIG) {
-        if (!has(cache, outputKey) && !fetchingKeys.has(outputKey) && sourceInputKeys.every(key => has(inputs, key))) {
-            try {
-                console.log('Fetching previous job outputs for:', outputKey);
+        if (!has(cache, outputKey) && !fetchingKeys.has(outputKey) && inputsSatisfied(sourceInputKeys, inputs)) {
+            const sourceInputs = sourceInputKeys.map(key => ({ key, data: inputs[key] }));
 
-                const sourceInputs = sourceInputKeys.map(key => ({ key, data: Storage.get('input', key) }));
-
-                fetchingKeys.add(outputKey);
-
-                const result = await sdk.getPreviousJobOutputs(sourceInputs, outputKey);
-                const cache = result.data && result.data[0];
-
-                if (cache) {
-                    Storage.set('cache', cache.key, cache.data);
-                }
-            } catch (error) {
-                console.error('failed to fetch cache', error);
-            } finally {
-                fetchingKeys.delete(outputKey);
-            }
+            promises.push(getAndCachePreviousOutput(sdk, sourceInputs, outputKey));
         }
     }
+
+    await Promise.all(promises);
 }
