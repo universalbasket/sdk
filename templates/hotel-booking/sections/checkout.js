bb14d888b435e81da49e8f3d0265502c99bfaae2
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-07-08 15:05:52 +0100

    Updates hotel booking templates to render themselves to document fragments.

--- templates/hotel-booking/sections/checkout.js
+++ templates/hotel-booking/sections/checkout.js
@@ -1,13 +1,16 @@
-import { html } from '/src/main.js';
+import { html } from '/web_modules/lit-html/lit-html.js';
+import render from '../render.js';
 import { payment } from '../inputs/index.js';
 
-export default (name, data) => html`
-    ${payment(data.otp)}
+export default function checkout(name, data) {
+    return render(html`
+        ${payment(data.otp)}
 
-    <div class="section__actions">
-        <button
-            type="button"
-            class="button button--right button--primary"
-            id="submit-btn-${name}">Pay</button>
-    </div>
-`;
+        <div class="section__actions">
+            <button
+                type="button"
+                class="button button--right button--primary"
+                id="submit-btn-${name}">Pay</button>
+        </div>
+    `);
+}
