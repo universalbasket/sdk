9f9d3cb645e71bde491d579f9f71747bc2d26379
muzafarova <alisapsee@gmail.com>
2019-07-08 14:56:53 +0100

    Fix selectedNoClaimsDiscountProtection template bug

--- templates/motor-insurance/inputs/selected-no-claims-discount-protection.js
+++ templates/motor-insurance/inputs/selected-no-claims-discount-protection.js
@@ -1,10 +1,13 @@
 import { html } from '/web_modules/lit-html/lit-html.js';
-// const key = 'selected-no-claims-discount-protection';
+const key = 'selected-no-claims-discount-protection';
 
 export default function selectedNoClaimsDiscountProtection(output) {
     if (!output) {
         return '';
     }
+
+    // TODO fix output's HTML and use SELECTEDPOLICYOPTION template instead
+    const protection = output[0];
 
     return html`
         <div class="field">
@@ -12,20 +15,19 @@ export default function selectedNoClaimsDiscountProtection(output) {
             <div class="field__inputs group group--merged">
                 <input
                     type="radio"
-                    name="selected-no-claims-discount-protection-object"
-                    id="selected-no-claims-discount-protection-yes"
-                    value="${ JSON.stringify(output) }"
+                    name="${key}-$object"
+                    id="${key}-yes"
+                    value="${ JSON.stringify(protection) }"
                     required
                     checked />
-                <label for="selected-no-claims-discount-protection-yes" class="button">Yes</label>
+                <label for="${key}-yes" class="button">Yes</label>
 
                 <input
                     type="radio"
-                    name="selected-no-claims-discount-protection-object"
-                    id="selected-no-claims-discount-protection-no"
+                    name="${key}-$object"
+                    id="${key}-no"
                     value="null" />
-                <label for="selected-no-claims-discount-protection-no" class="button">No</label>
+                <label for="${key}-no" class="button">No</label>
             </div>
-        </div>
     `;
 }
