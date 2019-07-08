2373b982ebfc304c3124e4b263a6e0925658936d
muzafarova <alisapsee@gmail.com>
2019-07-08 17:35:00 +0100

    Hotel main guest fix

--- templates/hotel-booking/sections/main-guest.js
+++ templates/hotel-booking/sections/main-guest.js
@@ -1,15 +1,19 @@
 import { html } from '/web_modules/lit-html/lit-html.js';
+import render from '../render.js';
 import { MainGuest } from '../inputs/index.js';
 
 // https://protocol.automationcloud.net/HotelBooking#MainGuest
-export default name => html`
-    <h2>Main guest details</h2>
-    ${MainGuest()}
 
-    <div class="section__actions">
-        <button
-            type="button"
-            class="button button--right button--primary"
-            id="submit-btn-${name}">Submit</button>
-    </div>
-`;
+export default function guest(name) {
+    return render(html`
+        <h2>Main guest details</h2>
+        ${MainGuest()}
+
+        <div class="section__actions">
+            <button
+                type="button"
+                class="button button--right button--primary"
+                id="submit-btn-${name}">Submit</button>
+        </div>
+    `);
+}
