63002706943868ded78aa28caa8a105e0c56300f
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-07-08 15:06:36 +0100

    Updates internal templates to render themselves to document fragments.

--- templates/internal/sections/tds-test.js
+++ templates/internal/sections/tds-test.js
@@ -1,31 +1,34 @@
-import { html } from '/src/main.js';
+import { html } from '/web_modules/lit-html/lit-html.js';
+import render from '../render.js';
 
-export default (name, _data) => html`
-    <div class="field">
-        <label class="field__name" for="[amount]">Amount</label>
-        <input
-            type="text"
-            name="[amount]"
-            value="10.00"
-            required />
-    </div>
+export default function tdsTest(name, _data) {
+    return render(html`
+        <div class="field">
+            <label class="field__name" for="[amount]">Amount</label>
+            <input
+                type="text"
+                name="[amount]"
+                value="10.00"
+                required />
+        </div>
 
-    <div class="field">
-        <label class="field__name" for="[pan-token]">PAN token</label>
-        <input
-            type="text"
-            name="[pan-token]"
-            required />
-    </div>
+        <div class="field">
+            <label class="field__name" for="[pan-token]">PAN token</label>
+            <input
+                type="text"
+                name="[pan-token]"
+                required />
+        </div>
 
-    <input type="hidden" name="[expiry-month]" value="12">
-    <input type="hidden" name="[expiry-year]" value="2019">
-    <input type="hidden" name="[security-code]" value="123">
+        <input type="hidden" name="[expiry-month]" value="12">
+        <input type="hidden" name="[expiry-year]" value="2019">
+        <input type="hidden" name="[security-code]" value="123">
 
-    <div class="section__actions">
-        <button
-            type="button"
-            class="button button--right button--primary"
-            id="submit-btn-${name}">Pay</button>
-    </div>
-`;
+        <div class="section__actions">
+            <button
+                type="button"
+                class="button button--right button--primary"
+                id="submit-btn-${name}">Pay</button>
+        </div>
+    `);
+}
