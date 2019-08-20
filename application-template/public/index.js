4c44be950ad94f45513c46824a3a6d4c7b3ba710
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-08-20 18:06:38 +0100

    Fixes the index of the application template.

--- application-template/public/index.js
+++ application-template/public/index.js
@@ -2,15 +2,28 @@ import { createApp } from '/web_modules/@ubio/sdk.js';
 import { createEndUserSdk } from '/web_modules/@ubio/client-library.js';
 import CONFIG from './ubio.config.js';
 
-async function createJob() {
-    // Make a request to your server to start a job and respond with:
-    // { jobId, serviceId, token, input }
+export async function createJob(/* will probably take arguments later */) {
+    const res = await fetch('/create-job', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({}) // Put stuff in here later if you need.
+    });
+
+    if (!res.ok) {
+        throw new Error(`Unexpected response from server ${res.status}: ${await res.text()}`);
+    }
+
+    return res.json();
 }
 
-createJob()
-    .then(({ token, jobId, serviceId, input }) => {
+export async function runJob({ token, jobId, serviceId, input, local }) {
+    try {
         const sdk = createEndUserSdk({ token, jobId, serviceId });
 
-        createApp({ mountPoint: window.app, sdk, input, ...CONFIG });
-    })
-    .catch(error => console.error(error));
+        createApp({ mountPoint: window.app, sdk, input, local, ...CONFIG });
+    } catch (error) {
+        console.error(error);
+    }
+}
