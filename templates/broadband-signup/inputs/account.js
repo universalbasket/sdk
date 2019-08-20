1456661b81937f369b249a4ab36480a40605c1f4
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-08-20 16:46:23 +0100

    Broadband account input need take no arguments.

--- templates/broadband-signup/inputs/account.js
+++ templates/broadband-signup/inputs/account.js
@@ -1,40 +1,36 @@
 import { html } from '/web_modules/lit-html/lit-html.js';
 
-export default function Account(prefix = 'account') {
+export default function Account() {
     return html`
-        <div name="${prefix}" class="filed-set">
+        <div name="account" class="filed-set">
             <div class="field filed-set">
-                <label
-                    class="field__name"
-                    for="${prefix}[email]">Email</label>
+                <label class="field__name" for="account[email]">Email</label>
                 <input
                     type="email"
-                    name="${prefix}[email]"
+                    name="account[email]"
                     placeholder="example@example.com"
                     pattern="[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}"
                     data-error="Please enter a valid email address"
-                    required />
+                    required>
             </div>
 
             <div class="field filed-set">
-                <label
-                    class="field__name"
-                    for="${prefix}[phone]">Mobile</label>
+                <label class="field__name" for="account[phone]">Mobile</label>
                 <input
                     type="hidden"
-                    name="${prefix}[phone][country-code]"
+                    name="account[phone][country-code]"
                     value="gb">
                 <input
                     type="tel"
-                    name="${prefix}[phone][number]"
+                    name="account[phone][number]"
                     placeholder="07912341234"
                     data-error="Please enter a valid phone number"
-                    required />
+                    required>
             </div>
 
             <div>
-                <input type="hidden" name="${prefix}[password]" value="">
-                <input type="hidden" name="${prefix}[is-existing-$boolean]" value="false">
+                <input type="hidden" name="account[password]" value="">
+                <input type="hidden" name="account[is-existing-$boolean]" value="false">
             </div>
         </div>`;
 }
