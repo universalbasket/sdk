a3bb752e1717c555d879c1c762b260b8d648e1c4
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-08-27 12:19:21 +0100

    Give templates access to storage and deprecate data field.

--- src/page-renderer.js
+++ src/page-renderer.js
@@ -2,8 +2,8 @@ import kebabcase from '/web_modules/lodash.kebabcase.js';
 import { serializeForm , getFormInputKeys } from './serialize-form.js';
 import defaultLoadingTemplate from './builtin-templates/loading.js';
 import flashError from './builtin-templates/flash-error.js';
-import * as Storage from './storage.js';
-import getDataForSection from './get-data-for-section.js';
+import { get as storageGet, getAll as storageGetAll, set as storageSet, del as storageDel } from './storage.js';
+import waitForDataForSection from './get-data-for-section.js';
 import { createInputs } from './main.js';
 import setupForm from './setup-form.js';
 
@@ -143,9 +143,9 @@ class PageRenderer {
         if (vaultIframe) {
             return submitVaultForm(this.sdk, vaultIframe)
                 .then(({ cardToken, panToken }) => {
-                    Storage.del('_', 'otp');
-                    Storage.set('_', 'cardToken', cardToken);
-                    Storage.set('_', 'panToken', panToken);
+                    storageDel('_', 'otp');
+                    storageSet('_', 'cardToken', cardToken);
+                    storageSet('_', 'panToken', panToken);
                     vaultIframe.classList.add('submitted');
                 });
         }
@@ -156,8 +156,8 @@ class PageRenderer {
         let panTokenSent = false;
 
         if (inputs.payment) {
-            const cardToken = Storage.get('_', 'cardToken');
-            const panToken = Storage.get('_', 'panToken');
+            const cardToken = storageGet('_', 'cardToken');
+            const panToken = storageGet('_', 'panToken');
 
             if (cardToken) {
                 inputs.payment.card = { '$token': cardToken };
@@ -177,11 +177,11 @@ class PageRenderer {
         return createInputs(this.sdk, inputs)
             .then(() => {
                 if (cardTokenSent) {
-                    Storage.del('_', 'cardToken');
+                    storageDel('_', 'cardToken');
                 }
 
                 if (panTokenSent) {
-                    Storage.del('_', 'panToken');
+                    storageDel('_', 'panToken');
                 }
             });
     }
@@ -227,22 +227,39 @@ class PageRenderer {
 
         this.scrollIntoView(sectionForm);
 
-        getDataForSection(waitFor)
-            .then(data => {
+        waitForDataForSection(waitFor)
+            .then(awaitedData => {
+                const renderer = this;
+
                 while (sectionForm.firstChild) {
                     sectionForm.removeChild(sectionForm.firstChild);
                 }
 
                 let skipped = false;
+                let warned = false;
 
-                const skip = () => {
-                    if (!skipped) {
-                        skipped = true;
-                        this.skipSection(name);
+                const rendered = template({
+                    name,
+                    get data() {
+                        if (!warned) {
+                            warned = true;
+                            console.warn('The template options "data" field is deprecated. Use "storage" instead.');
+                        }
+
+                        return awaitedData;
+                    },
+                    skip() {
+                        if (!skipped) {
+                            skipped = true;
+                            renderer.skipSection(name);
+                        }
+                    },
+                    sdk: this.sdk,
+                    inputFields: this.inputFields,
+                    storage: {
+                        get: storageGet
                     }
-                };
-
-                const rendered = template({ name, data, skip, sdk: this.sdk, inputFields: this.inputFields });
+                });
 
                 // Synchronous skipping means we can avoid rendering this section.
                 if (skipped) {
@@ -262,7 +279,7 @@ class PageRenderer {
     }
 
     skipIfSubmitted(name) {
-        const { inputs } = Storage.getAll();
+        const { inputs } = storageGetAll();
         const submittedInputKeys = Object.keys(inputs);
         const inputKeysInSection = getFormInputKeys(`#section-form-${name}`);
 
